264ac6376fb31cc5b7aa3ee51207392a3c154db7
Jakob Rolandsson <Kavzor@gmail.com>
2021-05-29 18:26:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 let search_btn = document.getElementById("search_random_btn");
-search_btn.addEventListener('click', event => checkPage("", ""))
+let item_holder = document.getElementById("displayed_item");
+let images = document.getElementById("images");
+items = [];
 
-let categories = ['items', 'non-player characters']
-
-var options = {
-    host: 'oldschool.runescape.wiki',
-    path: '/w/Special:Random/main'
-}
-
-
-function checkPage(category, url) {
-    if (categories.includes(category.toLowerCase())) {
-        console.log(url);
+search_btn.addEventListener('click', event => {
+    let item = items[Math.floor(Math.random() * items.length)];
+    item_holder.setAttribute("href", item.url);
+    item_holder.innerText = item.url
+    /*while (images.firstChild) {
+        images.removeChild(images.firstChild);
     }
-    else {
-        findPage(checkPage);
+    item.images.forEach(image => {
+        let img = document.createElement('img');
+        img.setAttribute('src', "https://oldschool.runescape.wiki" + image);
+        images.appendChild(img);
+    });*/
+});
+
+fetch("file.json")
+    .then(resp => resp.json())
+    .then(data => {
+        items = data;
+    });
+
+/*function loadFile() {
+    var file = new XMLHttpRequest();
+    file.open("GET", "file:///E:/dev/workspace/Evolve/buuptionary/file.txt");
+    file.onreadystatechange = function () {
+        if (file.readyState === 4) {
+            if (file.status === 200 || file.status == 0) {
+                items = file.responseText.split("\n");
+                console.log(items);
+            }
+        }
     }
+    file.send();
 }
 
-function findPage(callback) {
-    axios.get("https://oldschool.runescape.wiki/w/Special:Random/main", { headers: { "Access-Control-Allow-Origin": "*" } })
-        .then(resp => console.log(resp));
-    /*var request = http.request(options, function (res) {
-        var data = '';
-        res.on('data', function (chunk) {
-            data += chunk;
-        });
-        res.on('end', function () {
-            let doc = new jsdom.JSDOM(data);
-            let category = doc.window.document.querySelector('#mw-normal-catlinks ul');
-            console.log(category)
-            callback(category.firstChild.textContent, res.responseUrl)
-        });
-    });
-    request.on('error', function (e) {
-        console.log(e.message);
-    });
-    request.end();*/
-}
+setTimeout(loadFile, 10);*/
